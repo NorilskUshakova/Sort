22ef3d251327c45e85197343b7e429304127a213
NorilskUshakova <48198755+NorilskUshakova@users.noreply.github.com>
2019-05-25 11:16:55 +0500

    Добавьте файлы проекта.

---
+++ QuickSort/Sorter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public class Sorter
+    {
+        public static void QuickSort(int[] array)
+        {
+            if (array is null) return;
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        static void QuickSort(int[] a, int l, int r)
+        {
+            if (r <= l) return;
+            int k;
+            var v = a[r];
+            int i = l - 1, j = r, p = l - 1, q = r;
+            while (true)
+            {
+                while (a[++i] < v) ;
+                while (v < a[--j]) if (j == l) break;
+                if (i >= j) break;
+                Swap(a, i, j);
+                if (a[i] == v) { p++; Swap(a, p, i); }
+                if (v == a[j]) { q--; Swap(a, q, j); }
+            }
+            Swap(a,i, r);
+            j = i - 1;
+            i = i + 1;
+            for (k = l; k <= p; k++, j--)
+                Swap(a, k, j);
+            for (k = r - 1; k >= q; k--, i++)
+                Swap(a, k, i);
+            QuickSort(a, l, j);
+            QuickSort(a, i, r);
+        }
+
+        static void Swap(int[] array, int i, int j)
+        {
+            var e = array[i];
+            array[i] = array[j];
+            array[j] = e;
+        }
+    }
+}
---
+++ QuickSort/SorterTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace QuickSort
+{
+    [TestFixture]
+    class SorterTests
+    {
+        public void CheckSort(int[] array)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+                Assert.True(array[i] <= array[i + 1]);
+        }
+
+        [Test]
+        public void Sorted()
+        {
+            var array = new int[] { 1, 2, 3 };
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void Simple()
+        {
+            var array = new int[] { 2, 1 };
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void Five()
+        {
+            var array = new int[] { 5, 5, 5 };
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void Short()
+        {
+            var array = new int[] { 3 };
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void OneNumber()
+        {
+            var array = new int[10];
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void EmptyArray()
+        {
+            Sorter.QuickSort(new int[0]);
+        }
+
+        [Test]
+        public void NullArray()
+        {
+            Sorter.QuickSort(null);
+        }
+        
+        [Test]
+        public void Random()
+        {
+            var rnd = new Random();
+            var array = new int[1000];
+            for (var i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(-100, 100);
+            Sorter.QuickSort(array);
+            for(var i = 0; i < 10; i++)
+            {
+                var j = rnd.Next(0, array.Length - 2);
+                var k = rnd.Next(j, array.Length - 1);
+                Assert.GreaterOrEqual(array[k], array[j]);
+            }
+        }
+
+        [Test]
+        public void BigRandom()
+        {
+            var rnd = new Random();
+            var array = new int[10000000];
+            for (var i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(int.MinValue, int.MaxValue);
+            Sorter.QuickSort(array);
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                Assert.GreaterOrEqual(array[i + 1], array[i]);
+            }
+        }
+    }
+}
